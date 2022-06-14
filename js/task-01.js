a344609c5ea168d1200edd7d7598cb2457acb8be
Shako Yevhen <schako15101992@gmail.com>
2022-06-14 23:15:21 +0300

    Зробив 1 задачу

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const menuItemsByClass = document.querySelectorAll('.item');
+const menuItemlength = menuItemsByClass.length;
+
+console.log('Number of categories:', menuItemlength);
+
+
+const menuItemteg = document.querySelectorAll('h2');
+
+console.log(menuItemteg);
