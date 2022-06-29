7eb8d1700fe9faf21e7ce36260b9a0743ba8f237
Shako Yevhen <schako15101992@gmail.com>
2022-06-29 22:56:03 +0300

    Почав 5 задачу модуль 6

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputPlaceholderEll = document.querySelector('input[placeholder]');
+
+console.log(inputPlaceholderEll);
+
+const inputSpanEll = document.querySelector('span');
+console.log(inputSpanEll);
+
+inputSpanEll.textContent = 'dvwdvwd';
