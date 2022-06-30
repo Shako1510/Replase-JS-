d7f44984ab8c0696fbd6d14eed1078c9fff21b25
Shako Yevhen <schako15101992@gmail.com>
2022-06-30 19:43:08 +0300

    Модуль 6 завд 5 роблю

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,36 @@
-const inputPlaceholderEll = document.querySelector('input[placeholder]');
 
-console.log(inputPlaceholderEll);
+const inputEll = document.querySelector('input');
+console.log(inputEll);
+
+const placeholderEll = document.querySelector('[placeholder]');
+console.log(placeholderEll);
 
 const inputSpanEll = document.querySelector('span');
 console.log(inputSpanEll);
 
-inputSpanEll.textContent = 'dvwdvwd';
+
+
+inputEll.addEventListener('input', onInputChange);
+placeholderEll.addEventListener('input', onInputBlur);
+
+
+function onInputChange(event) {
+
+    console.log(event.currentTarget.value);
+
+    inputSpanEll.textContent = event.currentTarget.value;
+
+}
+
+
+inputEll.addEventListener('focus', onInputFocus);
+inputEll.addEventListener('blur', onInputBlur);
+
+function onInputFocus() {
+    inputEll.style.outlineColor = 'lightblue';
+    console.log('focus on input');
+}
+
+function onInputBlur() {
+    console.log('blur on input');
+}
