4db41530bf34cb5b8e675c92bf6371207d7a0842
Shako Yevhen <schako15101992@gmail.com>
2022-07-03 19:58:37 +0300

    Зробив задачу 5 мод д/з6

--- js/task-05.js
+++ js/task-05.js
@@ -2,35 +2,40 @@
 const inputEll = document.querySelector('input');
 console.log(inputEll);
 
-const placeholderEll = document.querySelector('[placeholder]');
-console.log(placeholderEll);
-
 const inputSpanEll = document.querySelector('span');
 console.log(inputSpanEll);
 
 
 
 inputEll.addEventListener('input', onInputChange);
-placeholderEll.addEventListener('input', onInputBlur);
+
 
 
 function onInputChange(event) {
 
-    console.log(event.currentTarget.value);
 
-    inputSpanEll.textContent = event.currentTarget.value;
 
+    if (event.currentTarget.value === '') {
+        inputSpanEll.textContent = 'Anonymous';
+
+    }
+
+    else {
+        inputSpanEll.textContent = event.currentTarget.value;
+    }
 }
 
 
-inputEll.addEventListener('focus', onInputFocus);
-inputEll.addEventListener('blur', onInputBlur);
+// Хотів стилізувати інпут при фокусі і блурі
+// inputEll.addEventListener('focus', onInputFocus);
+// inputEll.addEventListener('blur', onInputBlur);
 
-function onInputFocus() {
-    inputEll.style.outlineColor = 'lightblue';
-    console.log('focus on input');
-}
+// function onInputFocus() {
+//     inputEll.style.outlineColor = 'lightblue';
+//     console.log('focus on input');
+// }
 
-function onInputBlur() {
-    console.log('blur on input');
-}
+// function onInputBlur() {
+
+//     console.log('blur on input');
+// }
