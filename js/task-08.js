be3446f2bf8de106a301050460edfdc5c180b99a
Shako Yevhen <schako15101992@gmail.com>
2022-07-04 16:58:48 +0300

    Розпочав зад8 дз 6
    1) додав preventDefault
    2) додав alert

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+
+const formEll = document.querySelector('.login-form');
+console.log(formEll);
+
+
+formEll.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formListEll = event.currentTarget.elements;
+    console.log(formListEll);
+
+    if (!formEll.elements.email.value) {
+        alert('Заповни всі поля форми');
+    }
+
+
+
+};
