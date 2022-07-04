458782454757e614b596c1f43635059dadaf56d8
Shako Yevhen <schako15101992@gmail.com>
2022-07-04 12:45:36 +0300

    Завершив 6 завд 6 модуль

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,16 @@
 const inputEll = document.querySelector('#validation-input');
 
 console.log(inputEll);
-inputEll.classList.add('valid');
+// inputEll.classList.add('valid');
 
 
 
 inputEll.addEventListener('blur', event => {
-    if (event.currentTarget.value.length === inputEll.dataset.length) {
-        inputEll.classList.toggle('valid');
+    if (event.currentTarget.value.length === Number(inputEll.dataset.length)) {
+        inputEll.classList.add('valid');
         console.log('red', inputEll)
+    } else {
+        inputEll.classList.replace('valid', 'invalid');
+
     }
 });
