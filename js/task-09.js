85d4e80401ccd08f04fa83ca44d758ed0153429f
Shako Yevhen <schako15101992@gmail.com>
2022-07-08 12:33:06 +0300

    Завершив 9 дз 6 модуль

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const buttonEll = document.querySelector('button.change-color');
+console.log(buttonEll);
+
+const divEll = document.querySelector('.widget');
+console.log(divEll);
+
+const spanEll = document.querySelector('.color');
+console.log(spanEll);
+
+buttonEll.addEventListener('click', changeColorBody);
+
+function changeColorBody() {
+
+  divEll.style.backgroundColor = getRandomHexColor();
+  spanEll.textContent = getRandomHexColor();
+
+}
