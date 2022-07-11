1be2c1d6c2b414940313f1789253219d986cc2b8
Shako Yevhen <schako15101992@gmail.com>
2022-07-11 23:31:20 +0300

    Перероблене 8 дз,
    Замість formData і forEach, для збору данних з форми використав тепер
    elements

--- js/task-08.js
+++ js/task-08.js
@@ -11,17 +11,28 @@ function onFormSubmit(event) {
 
     const formListEll = event.currentTarget.elements;
 
-    const formData = new FormData(event.currentTarget);
+    const email = formListEll.email.value;
+    const password = formListEll.password.value;
+    const formData = {
+        email,
+        password,
+    };
+    // const formData = new FormData(event.currentTarget);
 
 
-    if (formEll.elements.email.value === '' || formEll.elements.password.value === '') {
+    if (email === '' || password === '') {
         alert('Заповни всі поля форми');
     } else {
-        formData.forEach((value, name) => {
-            console.log('onFormSubmit -> name', name);
-            console.log('onFormSubmit -> value', value);
-        })
+
+        // Варіант збору данних введених за доп formData, і перевірити значення через forEach.
+        // formData.forEach((value, name) => {
+        //     // console.log('onFormSubmit -> name', name);
+        //     // console.log('onFormSubmit -> value', value);
+        //     console.log(name, value);
+
+        // })
         console.log(formData);
+
     };
     return formEll.reset();
 };
