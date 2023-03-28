77d4fa97ff7209a47dcdf8a5d1049e22b9a370f1
Shako Yevhen <schako15101992@gmail.com>
2023-03-28 19:26:03 +0300

    Додав нову таску для тестів

--- js/task-11Repit.js
+++ js/task-11Repit.js
@@ -1,4 +1,11 @@
-const result = null && false || 'Mango';
+let result = (Math.random() * (3 - 0) + 0).toFixed(1);
+let result1 = 1.2;
 
+console.log(result)
+console.log(Math.round(result))
 
-console.log(result);
+let link = 'https://somesite.com/about';
+
+if () {
+
+}
